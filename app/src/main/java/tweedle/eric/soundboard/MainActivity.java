9afcbfddedb541ac399c2a9eef7d1ef13b660b7e
Eric-Tweedle <emtweedl@uwaterloo.ca>
2016-12-18 02:39:28 -0500

    it works, somewhat...

--- app/src/main/java/tweedle/eric/soundboard/MainActivity.java
+++ app/src/main/java/tweedle/eric/soundboard/MainActivity.java
@@ -1,6 +1,8 @@
 package tweedle.eric.soundboard;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
@@ -19,6 +21,8 @@ import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
 import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
+
+import java.io.IOException;
 
 import static tweedle.eric.soundboard.R.styleable.View;
 
@@ -39,7 +43,9 @@ public class MainActivity extends AppCompatActivity{
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
 
     }
-
+    int currentButton = 0;
+    boolean[] alreadySet = {false, false, false, false, false, false};
+    MediaPlayer[] sounds = new MediaPlayer[6];
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater findMenuItems = getMenuInflater();
@@ -63,7 +69,7 @@ public class MainActivity extends AppCompatActivity{
         }
     }
 
-    public void toggleRatingContainer(boolean showSelector){
+    public void toggleSelector(boolean showSelector){
         RelativeLayout selector = (RelativeLayout) findViewById(R.id.selector);
         if (showSelector) {
             selector.setVisibility(android.view.View.VISIBLE);
@@ -73,34 +79,123 @@ public class MainActivity extends AppCompatActivity{
         }
     }
     public void Go(View view){
-        toggleRatingContainer(true);
         TextView mtext = (TextView) findViewById(R.id.selectorText);
-        switch (view.getId()){
+        switch (view.getId()) {
             case R.id.imageButton1:
                 mtext.setText("button 1");
+                currentButton = 0;
+
                 break;
             case R.id.imageButton2:
                 mtext.setText("button 2");
+                currentButton = 1;
                 break;
             case R.id.imageButton3:
                 mtext.setText("button 3");
+                currentButton = 2;
                 break;
             case R.id.imageButton4:
                 mtext.setText("button 4");
+                currentButton = 3;
                 break;
             case R.id.imageButton5:
                 mtext.setText("button 5");
+                currentButton = 4;
                 break;
             case R.id.imageButton6:
                 mtext.setText("button 6");
+                currentButton = 5;
                 break;
             default:
-                throw new RuntimeException("Unknow button ID");
+                throw new RuntimeException("Unknown button ID");
         }
+        if (alreadySet[currentButton]){
+            sounds[currentButton].start();
+        }
+        else{
+            toggleSelector(true);
+        }
+
     }
     public void onBackPressed(){
-        toggleRatingContainer(false);
+        toggleSelector(false);
     }
+    public void done(View view) { toggleSelector(false);}
+    String currentType;
+    private static final int READ_REQUEST_CODE = 42;
+
+    public void performFileSearch(View view) {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("image/*");
+        currentType = "image";
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode,
+                                 Intent resultData) {
+
+        // The ACTION_OPEN_DOCUMENT intent was sent with the request code
+        // READ_REQUEST_CODE. If the request code seen here doesn't match, it's the
+        // response to some other intent, and the code below shouldn't run at all.
+
+        if (requestCode == READ_REQUEST_CODE && resultCode == Activity.RESULT_OK) {
+            // The document selected by the user won't be returned in the intent.
+            // Instead, a URI to that document will be contained in the return intent
+            // provided to this method as a parameter.
+            // Pull that URI using resultData.getData().
+            Uri uri = null;
+            if (resultData != null) {
+                ImageButton btn1 = (ImageButton) findViewById(R.id.imageButton1);
+                ImageButton btn2 = (ImageButton) findViewById(R.id.imageButton2);
+                ImageButton btn3 = (ImageButton) findViewById(R.id.imageButton3);
+                ImageButton btn4 = (ImageButton) findViewById(R.id.imageButton4);
+                ImageButton btn5 = (ImageButton) findViewById(R.id.imageButton5);
+                ImageButton btn6 = (ImageButton) findViewById(R.id.imageButton6);
+                ImageButton[] arr ={btn1, btn2, btn3, btn4, btn5, btn6};
+                if (currentType.equals("image")) {
+                    arr[currentButton].setImageURI(resultData.getData());
+                }
+                else if (currentType.equals("audio")){
+                    sounds[currentButton] = MediaPlayer.create(this, resultData.getData());
+                    alreadySet[currentButton] = true;
+                }
+            }
+        }
+    }
+
+    public void performFileSearchSound(View view) {
+
+        // ACTION_OPEN_DOCUMENT is the intent to choose a file via the system's file
+        // browser.
+        Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+
+        // Filter to only show results that can be "opened", such as a
+        // file (as opposed to a list of contacts or timezones)
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+
+        // Filter to show only images, using the image MIME data type.
+        // If one wanted to search for ogg vorbis files, the type would be "audio/ogg".
+        // To search for all documents available via installed storage providers,
+        // it would be "*/*".
+        intent.setType("audio/*");
+        currentType = "audio";
+
+        startActivityForResult(intent, READ_REQUEST_CODE);
+    }
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
