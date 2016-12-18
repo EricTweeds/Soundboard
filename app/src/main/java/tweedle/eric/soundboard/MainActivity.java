ad80dc794453fc023085765c7e90ce9732986a26
Eric-Tweedle <emtweedl@uwaterloo.ca>
2016-12-18 00:51:21 -0500

    first commit

---
+++ app/src/main/java/tweedle/eric/soundboard/MainActivity.java
@@ -0,0 +1,139 @@
+package tweedle.eric.soundboard;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
+
+import static tweedle.eric.soundboard.R.styleable.View;
+
+public class MainActivity extends AppCompatActivity{
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater findMenuItems = getMenuInflater();
+        findMenuItems.inflate(R.menu.bar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.settings:
+                Intent intent = new Intent(this, MainActivity.class);
+                startActivity(intent);
+                return true;
+
+            default:
+                // If we got here, the user's action was not recognized.
+                // Invoke the superclass to handle it.
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    public void toggleRatingContainer(boolean showSelector){
+        RelativeLayout selector = (RelativeLayout) findViewById(R.id.selector);
+        if (showSelector) {
+            selector.setVisibility(android.view.View.VISIBLE);
+        }
+        else{
+            selector.setVisibility(android.view.View.GONE);
+        }
+    }
+    public void Go(View view){
+        toggleRatingContainer(true);
+        TextView mtext = (TextView) findViewById(R.id.selectorText);
+        switch (view.getId()){
+            case R.id.imageButton1:
+                mtext.setText("button 1");
+                break;
+            case R.id.imageButton2:
+                mtext.setText("button 2");
+                break;
+            case R.id.imageButton3:
+                mtext.setText("button 3");
+                break;
+            case R.id.imageButton4:
+                mtext.setText("button 4");
+                break;
+            case R.id.imageButton5:
+                mtext.setText("button 5");
+                break;
+            case R.id.imageButton6:
+                mtext.setText("button 6");
+                break;
+            default:
+                throw new RuntimeException("Unknow button ID");
+        }
+    }
+    public void onBackPressed(){
+        toggleRatingContainer(false);
+    }
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Main Page") // TODO: Define a title for the content shown.
+                // TODO: Make sure this auto-generated URL is correct.
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
+    }
+}
